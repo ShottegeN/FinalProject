dde27e187e6148cf12bb48e62d2df3c64e9d42cd
ShottegeN <128291624+ShottegeN@users.noreply.github.com>
2024-10-31 08:34:47 +0200

    Adding more controllers

---
+++ WebProject/ToyShop.Web/Controllers/OrderController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ToyShop.Web.Models;
+
+namespace ToyShop.Web.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly ILogger<OrderController> _logger;
+
+        public OrderController(ILogger<OrderController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
