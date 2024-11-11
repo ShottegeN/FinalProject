e84ebad14c9effb5bfc0e0d9d329d69b0737788c
ShottegeN <128291624+ShottegeN@users.noreply.github.com>
2024-11-11 11:57:39 +0200

    Adding more methods in the repository

--- WebProject/ToyShop.Data/Common/IRepository.cs
+++ WebProject/ToyShop.Data/Common/IRepository.cs
@@ -3,15 +3,14 @@
     public interface IRepository : IDisposable
     {
         IQueryable<T> All<T>() where T : class;
-
         IQueryable<T> AllReadonlyAsync<T>() where T : class;
-
         Task<T?> GetByIdAsync<T>(object id) where T : class;
-
         Task AddAsync<T>(T entity) where T : class;
-
         Task AddRangeAsync<T>(IEnumerable<T> entities) where T : class;
-
+        Task UpdateAsync<T>(T entity) where T : class;
+        Task RemoveByIdAsync<T>(object id) where T : class;
+        Task RemoveAsync<T>(T entity) where T : class;
+        Task RemoveRangeAsync<T>(IEnumerable<T> entities) where T : class;
         Task<int> SaveChangesAsync();
     }
 }
--- WebProject/ToyShop.Data/Common/Repository.cs
+++ WebProject/ToyShop.Data/Common/Repository.cs
@@ -18,14 +18,12 @@ namespace ToyShop.Data.Common
 
         public async Task AddAsync<T>(T entity) where T : class
         {
-            await DbSet<T>()
-                .AddAsync(entity);
+            await DbSet<T>().AddAsync(entity);
         }
 
         public async Task AddRangeAsync<T>(IEnumerable<T> entities) where T : class
         {
-            await DbSet<T>()
-                .AddRangeAsync(entities);
+            await DbSet<T>().AddRangeAsync(entities);
         }
 
         public IQueryable<T> All<T>() where T : class
@@ -35,24 +33,49 @@ namespace ToyShop.Data.Common
 
         public IQueryable<T> AllReadonlyAsync<T>() where T : class
         {
-            return DbSet<T>()
-                .AsNoTracking();
-        }
-
-        public void Dispose()
-        {
-            Context.Dispose();
+            return DbSet<T>().AsNoTracking();
         }
 
         public async Task<T?> GetByIdAsync<T>(object id) where T : class
         {
-            return await DbSet<T>()
-                .FindAsync(id);
+            return await DbSet<T>().FindAsync(id);
+        }
+
+        public Task UpdateAsync<T>(T entity) where T : class
+        {
+            DbSet<T>().Update(entity);
+            return Task.CompletedTask;
+        }
+
+        public async Task RemoveByIdAsync<T>(object id) where T : class
+        {
+            var entity = await GetByIdAsync<T>(id);
+            if (entity != null)
+            {
+                DbSet<T>().Remove(entity);
+            }
+        }
+
+        public Task RemoveAsync<T>(T entity) where T : class
+        {
+            DbSet<T>().Remove(entity);
+            return Task.CompletedTask;
+        }
+
+        public Task RemoveRangeAsync<T>(IEnumerable<T> entities) where T : class
+        {
+            DbSet<T>().RemoveRange(entities);
+            return Task.CompletedTask;
         }
 
         public async Task<int> SaveChangesAsync()
         {
             return await Context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            Context.Dispose();
         }
     }
 }
