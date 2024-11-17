10af8168c08bf9490c04e67da8f94141347f8212
ShottegeN <128291624+ShottegeN@users.noreply.github.com>
2024-11-17 13:07:52 +0200

    Some HTML Changes

--- WebProject/ToyShop.Web/wwwroot/js/site.js
+++ WebProject/ToyShop.Web/wwwroot/js/site.js
@@ -2,16 +2,3 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-document.addEventListener('DOMContentLoaded', function () {
-    const homeLi = document.querySelector('.homeLi');  // Select the first <li> with class 'homeLi'
-    const otherNavItems = document.querySelectorAll('ul.main-nav li:not(.homeLi)');  // Select all other <li> elements
-
-    // Add click event to the homeLi
-    if (homeLi) {
-        homeLi.addEventListener('click', function () {
-            // Ensure the homeLi gets the 'active' class
-            this.classList.remove('active');
-        });
-    }    
-    console.log('site.js is loaded!');
-});
